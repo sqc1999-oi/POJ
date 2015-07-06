cff062ae4b52b958e4b515f7965d66a0d651a4c3
办公室 <sunqiancheng0644@126.com>
2015-07-06 18:29:33 +0800

    Solve 2456.

--- 2456 Aggressive cows/源.cpp
+++ 2456 Aggressive cows/源.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int x[100001];
+int a[100001];
 int main()
 {
 	ios::sync_with_stdio(false);
 	int n, c;
 	cin >> n >> c;
-	for (int i = 1; i <= n; i++) cin >> x[i];
-	sort(x + 1, x + 1 + n);
-	int l = 0, r = x[n], ans = 0;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	sort(a + 1, a + 1 + n);
+	int l = 0, r = a[n], ans = 0;
 	while (l <= r)
 	{
-
+		int mid = (l + r) / 2;
+		int cnt = 1, len = 0;
+		for (int i = 2; i <= n; i++)
+		{
+			len += a[i] - a[i - 1];
+			if (len >= mid)
+			{
+				cnt++;
+				len = 0;
+			}
+		}
+		if (cnt < c) r = mid - 1;
+		else
+		{
+			ans = mid;
+			l = mid + 1;
+		}
 	}
+	cout << ans;
 }
