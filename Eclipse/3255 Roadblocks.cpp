8ebb1966fa761b6550ef7812566c98a04102ed4d
机房 <sunqiancheng0644@126.com>
2015-07-07 10:25:37 +0800

    Add 3255, 3623

---
+++ Eclipse/3255 Roadblocks.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+using namespace std;
+struct Node
+{
+	int From, To, Pow, Next;
+} E[200001];
+int G[5001], cnt;
+void AddEdge(int from, int to, int pow)
+{
+	E[++cnt] = Node { from, to, pow, G[from] };
+	G[from] = cnt;
+}
+int dis[2][5001];
+bool inq[5001];
+void Spfa(int s, int f)
+{
+	memset(dis[f], 0x3f, sizeof dis[f]);
+	dis[f][s] = 0;
+	inq[s] = true;
+	queue<int> q;
+	q.push(s);
+	while (!q.empty())
+	{
+		int u = q.front();
+		q.pop();
+		inq[u] = false;
+		for (int i = G[u]; i != 0; i = E[i].Next)
+		{
+			int tmp = dis[f][E[i].To];
+			dis[f][E[i].To] = min(dis[f][E[i].To], dis[f][u] + E[i].Pow);
+			if (tmp != dis[f][E[i].To] && !inq[E[i].To])
+			{
+				q.push(E[i].To);
+				inq[E[i].To] = true;
+			}
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n, r;
+	cin >> n >> r;
+	for (int i = 1; i <= r; i++)
+	{
+		int a, b, d;
+		cin >> a >> b >> d;
+		AddEdge(a, b, d);
+		AddEdge(b, a, d);
+	}
+	Spfa(1, 0);
+	Spfa(n, 1);
+	int ans = 0x3f3f3f3f;
+	for (int i = 1; i <= cnt; i++)
+	{
+		int x = dis[0][E[i].From] + E[i].Pow + dis[1][E[i].To];
+		if (x > dis[0][n] && x < ans) ans = x;
+	}
+	cout << ans;
+}
---
+++ Eclipse/3623 Best Cow Line, Gold.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+char ch[30001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> ch[i];
+	int l = 1, r = n, cnt = 0;
+	while (l <= r)
+	{
+		int i = l, j = r;
+		for (; i <= j && ch[i] == ch[j]; i++, j--);
+		if (ch[i] < ch[j]) cout << ch[l++];
+		else cout << ch[r--];
+		if (++cnt == 80)
+		{
+			cout << endl;
+			cnt = 0;
+		}
+	}
+}
