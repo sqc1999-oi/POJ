f57356ce43f4b427e87a6d8abe8c97f31e8c69b5
机房 <sunqiancheng0644@126.com>
2015-07-07 14:45:05 +0800

    Add 3253.

---
+++ Eclipse/3253 Fence Repair.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	priority_queue<long long, vector<long long>, greater<long long> > q;
+	for (int i = 1; i <= n; i++)
+	{
+		int x;
+		cin >> x;
+		q.push(x);
+	}
+	long long ans = 0;
+	while (q.size() > 1)
+	{
+		long long x = q.top();
+		q.pop();
+		long long y = q.top();
+		q.pop();
+		ans += x + y;
+		q.push(x + y);
+	}
+	cout << ans;
+}
